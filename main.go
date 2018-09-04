47d83b9ca14b27f32878edf149bee3947b011aa5
Dede Lamb <dede.lamb@myob.com>
2018-09-04 11:02:54 +1000

    watch file and re render

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/google/go-github/github"
 )
@@ -17,9 +18,37 @@ type client struct {
 	src  string
 	ctx  string
 	html string
+	css  string
+	mod  time.Time
+}
+
+func (c *client) init() error {
+	const url = "https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css"
+	rsp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer rsp.Body.Close()
+
+	css, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return err
+	}
+
+	c.css = string(css)
+	return nil
 }
 
 func (c *client) load() error {
+	s, err := os.Stat(c.src)
+	if err != nil {
+		return err
+	}
+	if c.mod == s.ModTime() {
+		return nil
+	}
+	c.mod = s.ModTime()
+
 	md, err := ioutil.ReadFile(c.src)
 	if err != nil {
 		return err
@@ -33,6 +62,7 @@ func (c *client) load() error {
 		opts.Mode = "gdm"
 	}
 
+	log.Print("rendering " + c.src)
 	html, _, err := c.Markdown(context.Background(), string(md), opts)
 	if err != nil {
 		return err
@@ -54,13 +84,36 @@ func main() {
 		ctx:    *ctx,
 	}
 
-	if err := c.load(); err != nil {
+	if err := c.init(); err != nil {
 		log.Fatal(err)
 	}
 
+	go func() {
+		t := time.NewTicker(100 * time.Millisecond)
+		for range t.C {
+			if err := c.load(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, c.html)
+		fmt.Fprintf(w, page, c.css, c.html)
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *prt), nil))
 }
+
+const page = `<html>
+<head>
+<style>
+%s
+</style>
+</head>
+<body>
+<article class="markdown-body">
+%s
+</article>
+</body>
+</html>
+`
